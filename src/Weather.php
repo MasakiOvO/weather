dfd18c531983758097cf2ac5a088d35d1c4fd42f
maxiao <xiaoma1@linkdoc.com>
2021-03-15 14:30:25 +0800

    first commit

---
+++ src/Weather.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Overtrue\Weather;
+
+use GuzzleHttp\Client;
+use Overtrue\Weather\Exceptions\HttpException;
+use Overtrue\Weather\Exceptions\InvaildArgumentException;
+
+class Weather
+{
+    protected $key;
+
+    protected $guzzleOptions = [];
+
+    public function __construct($key)
+    {
+        $this->key = $key;
+    }
+
+    public function setGuzzleOptions(array $options)
+    {
+        $this->guzzleOptions = $options;
+    }
+
+    public function getHttpClient()
+    {
+        return new Client($this->guzzleOptions);
+    }
+
+    public function getWeather($city, $type = 'base', $format = 'json')
+    {
+        $url = 'https://restapi.amap.com/v3/weather/weatherInfo';
+
+        if(!in_array(strtolower($format),['xml', 'json'])) {
+            throw new InvaildArgumentException('Invaild response format: ' . $format);
+        }
+
+        if(!in_array(strtolower($type),['base', 'all'])) {
+            throw new InvaildArgumentException('Invaild type value(base/all): ' . $type);
+        }
+
+
+        $query = array_filter([
+            'key' => $this->key,
+            'city' => $city,
+            'output' => $format,
+            'extensions' => $type
+        ]);
+
+        try {
+            $response = $this->getHttpClient()->get($url, [
+                'query' => $query,
+            ])->getBody()->getContents();
+            return 'json' === $format ? \json_decode($response, true) : $response;
+        } catch (\Exception $e) {
+            throw new HttpException($e->getMessage(), $e->getCode(), $e);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tests/WeatheaTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Overtrue\Weather\Tests;
+
+use Overtrue\Weather\Exceptions\InvaildArgumentException;
+use Overtrue\Weather\Weather;
+use PHPUnit\Framework\TestCase;
+
+class WeatheaTest extends TestCase
+{
+    public function testGetWeather()
+    {
+
+    }
+
+    public function testGetHttpClient()
+    {
+
+    }
+
+    public function testSetGuzzleOptinos()
+    {
+
+    }
+
+    public function testGetWeatherWithInvalidType()
+    {
+         $w = new Weather('mock-key');
+         $this->expectException(InvaildArgumentException::class);
+         $this->expectExceptionMessage('Invalid type value(base/all): foo');
+         $w->getWeather('深圳','foo');
+
+         $this->fail('Failed to assert getWeather throw exception with invalid argument.');
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
